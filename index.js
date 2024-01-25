bdf2fb66bdd13a1792343d2b7f6362b2efaa3559
Philipp Schrittwieser <123021085+Philipp-Schrittwieser@users.noreply.github.com>
2024-01-25 11:29:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+      <meta charset="UTF-8">
+      <title>Google Weiterleitung</title>
+      <meta http-equiv="refresh" content="0;url=https://www.willhaben.at/iad/gebrauchtwagen/auto/gebrauchtwagenboerse?sfId=bff2225e-6c94-49fd-adc9-4b9b321e18d1&isNavigation=true&page=1&keyword=&PRICE_TO=19000&MILEAGE_TO=250000&YEAR_MODEL_FROM=2009">
+  </head>
+  <body>
+    <h1>About to forward...</h1>
+  </body>
+</html>
